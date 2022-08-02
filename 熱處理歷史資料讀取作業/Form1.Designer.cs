4e7e3d40a9c024a5a09b327ce55814c3f9f1b262
hk <hk@04>
2022-08-02 09:20:43 +0800

    加入專案檔案。

---
+++ 熱處理歷史資料讀取作業/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace 熱處理歷史資料讀取作業
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(876, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "資料讀取";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1086, 391);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1002, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "匯出EXCEL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton2.Location = new System.Drawing.Point(110, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(93, 20);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "office2016";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton1.Location = new System.Drawing.Point(6, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(93, 20);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "office2007";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(661, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(209, 56);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "OFFICE";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(446, 16);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(209, 57);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "LOAD";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton3.Location = new System.Drawing.Point(15, 24);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(89, 20);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "生產記錄";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton4.Location = new System.Drawing.Point(110, 24);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(89, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "電力記錄";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.dateTimePicker1);
+            this.groupBox4.Location = new System.Drawing.Point(36, 40);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(189, 53);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Choose Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 20);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(160, 27);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(87, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1899年12月31日";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(36, 1);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(386, 38);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Now Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label3.Location = new System.Drawing.Point(458, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "熱處理1",
+            "熱處理2"});
+            this.comboBox1.Location = new System.Drawing.Point(8, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(188, 24);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "熱處理1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.comboBox1);
+            this.groupBox5.Font = new System.Drawing.Font("新細明體", 9F);
+            this.groupBox5.Location = new System.Drawing.Point(231, 40);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 53);
+            this.groupBox5.TabIndex = 13;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Production Line";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1166, 507);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
+
---
+++ 熱處理歷史資料讀取作業/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+
+
+namespace 熱處理歷史資料讀取作業
+{
+    public partial class Form1 : Form
+    {
+
+        public static DateTime minTime = DateTime.Now ;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = true;
+            timer1.Enabled = true;
+            dateTimePicker1.Value = DateTime.Today.AddDays(-1);
+
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy/MM/dd";
+
+            
+
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DataTable DataTable = new DataTable();
+            DataTable.Rows.Clear();
+            dataGridView1.DataSource = DataTable;
+
+            
+            minTime = dateTimePicker1.Value;
+
+            
+       
+          
+                try
+                {
+                    if (radioButton3.Checked == true)
+                    {
+                        string[] dirs = new string[0];
+                        if (comboBox1.Text == "熱處理1")
+                        {
+                            dirs = Directory.GetFiles(@"\\192.168.18.1\s7034be\MDB\" + minTime.ToString("yyyy") + @"\" + minTime.ToString("MM") + @"\", "HIS" + minTime.ToString("dd") + ".mdb");
+                        }
+                        else if (comboBox1.Text == "熱處理2")
+                        {
+                            dirs = Directory.GetFiles(@"\\192.168.18.2\s7033be\MDB\" + minTime.ToString("yyyy") + @"\" + minTime.ToString("MM") + @"\", "HIS" + minTime.ToString("dd") + ".mdb");
+
+                        }
+                        string file = @" ";
+
+                        foreach (var HIS in dirs)
+                        {
+                            file = HIS.ToString();
+                            label3.Text = HIS.ToString();
+                        }
+
+                        //Console.WriteLine(file);
+
+                        string l_provider = "Microsoft.Jet.OLEDB.4.0";
+
+                        if (radioButton1.Checked == true)
+                            l_provider = "Microsoft.Jet.OLEDB.4.0";
+                        else if (radioButton2.Checked == true)
+                            l_provider = "Microsoft.ACE.OLEDB.12.0";
+
+                        OleDbConnection connection = new OleDbConnection("Provider=" + l_provider + "; Data Source=" + file + ";jet oledb:database password=;");
+
+                        connection.Open();
+                        //MessageBox.Show("開啟動作後，連結狀態：" + connection.State.ToString(), "連結狀態");
+
+                        String queryString;
+                        string[] a = new string[0];
+
+                        queryString = "Select * from HISTORY where M_NO is not null and [L-T] like '%00' order by [L-T];";
+
+
+
+                        OleDbCommand command = new OleDbCommand(queryString, connection);
+                        OleDbDataAdapter dataAdpter = new OleDbDataAdapter(queryString, connection);
+                        DataSet DataSetValue = new DataSet();
+                        DataSetValue.Clear();
+                        dataAdpter.Fill(DataSetValue);
+
+                        DataTable = new DataTable();
+
+                        //Console.WriteLine(DataSetValue.Tables[0].Columns[0].Caption);
+
+
+                        for (int i = 0; i < DataSetValue.Tables[0].Columns.Count; i++)
+                        {
+                            DataTable.Columns.Add(DataSetValue.Tables[0].Columns[i].Caption);
+
+                        }
+
+                        //Console.WriteLine(DataSetValue.Tables[0].Columns.Count);
+                        Array.Resize(ref a, DataSetValue.Tables[0].Columns.Count);
+
+                        for (int i = 0; i < DataSetValue.Tables[0].Rows.Count; i++)
+                        {
+                            for (int j = 0; j < DataSetValue.Tables[0].Columns.Count; j++)
+                            {
+                                //Console.WriteLine(DataSetValue.Tables[0].Rows[i][DataSetValue.Tables[0].Columns[j].Caption].ToString());
+                                a[j] = DataSetValue.Tables[0].Rows[i][DataSetValue.Tables[0].Columns[j].Caption].ToString();
+                                if (j == 0)
+                                {
+                                    DateTime DateObject = DateTime.Parse(a[0]);
+                                    a[0] = DateObject.ToString("yyyy/MM/dd HH:mm:ss");
+                                    
+                                }
+
+                            }
+                            DataTable.Rows.Add(a);
+                        }
+
+                        
+
+                        dataGridView1.DataSource = DataTable;
+                        dataGridView1.AllowUserToAddRows = false;
+
+                        //自動調整寬度
+                        dataGridView1.AutoResizeColumns();
+
+                        //queryString = "INSERT INTO DATA ([Value1], [Value2]) VALUES ('" + Value1.Text + "','" + Value2.Text + "')";
+                        //command = new OleDbCommand(queryString, connection);
+                        //Console.WriteLine(dataGridView1.RowCount);
+
+                        connection.Close();
+                    }
+                    else if (radioButton4.Checked == true)
+                    {
+
+                        string[] dirs = new string[0];
+                        if (comboBox1.Text == "熱處理1")
+                        {
+                            dirs = Directory.GetFiles(@"\\192.168.18.1\s7034be\MDB\" + minTime.ToString("yyyy") + @"\" + minTime.ToString("MM") + @"\", "HISDMP" + minTime.ToString("dd") + ".mdb");
+                        }
+                        else if (comboBox1.Text == "熱處理2")
+                        {
+                            dirs = Directory.GetFiles(@"\\192.168.18.2\s7033be\MDB\" + minTime.ToString("yyyy") + @"\" + minTime.ToString("MM") + @"\", "HISDMP" + minTime.ToString("dd") + ".mdb");
+
+                        }
+                        string file = @" ";
+
+                        foreach (var HIS in dirs)
+                        {
+                            file = HIS.ToString();
+                            label3.Text = HIS.ToString();
+                        }
+
+                        string l_provider = "Microsoft.Jet.OLEDB.4.0";
+
+                        if (radioButton1.Checked == true)
+                            l_provider = "Microsoft.Jet.OLEDB.4.0";
+                        else if (radioButton2.Checked == true)
+                            l_provider = "Microsoft.ACE.OLEDB.12.0";
+
+                        OleDbConnection connection = new OleDbConnection("Provider=" + l_provider + "; Data Source=" + file + ";jet oledb:database password=;");
+
+                        connection.Open();
+                        //MessageBox.Show("開啟動作後，連結狀態：" + connection.State.ToString(), "連結狀態");
+
+                        String queryString;
+                        string[] a = new string[0];
+
+                        queryString = "Select * from DPM where H_V <> 0 and [L-T] in (select min([L-T]) from DPM group by H_HOUR,H_MIN) order by [L-T];";
+
+
+                        OleDbCommand command = new OleDbCommand(queryString, connection);
+                        OleDbDataAdapter dataAdpter = new OleDbDataAdapter(queryString, connection);
+                        DataSet DataSetValue = new DataSet();
+                        DataSetValue.Clear();
+                        dataAdpter.Fill(DataSetValue);
+
+                        DataTable = new DataTable();
+
+                        //Console.WriteLine(DataSetValue.Tables[0].Columns[0].Caption);
+
+
+                        for (int i = 0; i < DataSetValue.Tables[0].Columns.Count; i++)
+                        {
+                            DataTable.Columns.Add(DataSetValue.Tables[0].Columns[i].Caption);
+
+                        }
+
+                        //Console.WriteLine(DataSetValue.Tables[0].Columns.Count);
+                        Array.Resize(ref a, DataSetValue.Tables[0].Columns.Count);
+
+                        for (int i = 0; i < DataSetValue.Tables[0].Rows.Count; i++)
+                        {
+                            for (int j = 0; j < DataSetValue.Tables[0].Columns.Count; j++)
+                            {
+                                //Console.WriteLine(DataSetValue.Tables[0].Rows[i][DataSetValue.Tables[0].Columns[j].Caption].ToString());
+                                a[j] = DataSetValue.Tables[0].Rows[i][DataSetValue.Tables[0].Columns[j].Caption].ToString();
+                                if (j == 0)
+                                {
+                                    DateTime DateObject = DateTime.Parse(a[0]);
+                                    a[0] = DateObject.ToString("yyyy/MM/dd HH:mm:ss");
+
+                                }
+                                
+                            }
+                             DataTable.Rows.Add(a); 
+                            
+                        }
+
+                        
+                        dataGridView1.DataSource = DataTable;
+                        dataGridView1.AllowUserToAddRows = false;
+
+                        //自動調整寬度
+                        dataGridView1.AutoResizeColumns();
+
+                        //queryString = "INSERT INTO DATA ([Value1], [Value2]) VALUES ('" + Value1.Text + "','" + Value2.Text + "')";
+                        //command = new OleDbCommand(queryString, connection);
+                        //Console.WriteLine(dataGridView1.RowCount);
+
+                        connection.Close();
+                    }
+
+                }
+                catch (System.IO.DirectoryNotFoundException)
+                {
+                    MessageBox.Show("無當天紀錄");
+                }
+               
+        }
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFD = new SaveFileDialog();
+            saveFD.InitialDirectory = "C:";
+            saveFD.Title = "Save as Excel File";
+            saveFD.FileName = "";
+            saveFD.Filter = "Excel File(2003)|*.xls|Excel File(2007)|*.xlsx";
+            if (saveFD.ShowDialog() != DialogResult.Cancel)
+            {
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Application.Workbooks.Add(Type.Missing);
+
+                excel.get_Range("A:AZ", Type.Missing).NumberFormatLocal = "@";  //設定A-AE欄儲存格格式為文字
+                for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+                {
+                    excel.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+                }
+                for (int i = 0; i < dataGridView1.Rows.Count; i++) 
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        if (dataGridView1[j, i].Value == null)
+                        {
+                            this.dataGridView1[j, i].Value = "0";
+                        }
+                        string value = dataGridView1[j, i].Value.ToString();
+                        excel.Cells[i + 2, j + 1] = value;
+                    }
+                }
+                excel.ActiveWorkbook.SaveCopyAs(saveFD.FileName.ToString());
+                excel.ActiveWorkbook.Saved = true;
+                excel.Quit();
+            }
+            MessageBox.Show("EXCEL產製完成");
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            label1.Text = dt.ToString("yyyy/MM/dd tt hh:mm:ss");
+        }
+
+       
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            if (dateTimePicker1.Value.Date >= DateTime.Today)
+            {
+                MessageBox.Show("當日資料無法讀取");
+                dateTimePicker1.Value = DateTime.Today.AddDays(-1);
+                return;
+            }
+            
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
